3a47c0c6955a15cd9db465eab2e6a020a8e35841
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-05-09 01:05:14 +0300

    Start

---
+++ C#/ConsoleApp1/02_Variables/program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class variables
+    {
+        static void Main(string[] args)
+        {
+            int x;  // declaration.
+            x = 1337;  // initialization.
+            int y = 69;  // declaration + initialization.
+            int z = x + y;
+            int age = 30;
+
+            double height = 50.5; // double == float number.
+
+            bool alive = false; // bool == Boolean.
+
+            char symbol = '@'; // char == character (singular) character must be within single quotes.
+
+            String name = "N/A"; // String starts with capital S.
+            String UserName = symbol + name;
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Height = " + height);
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your username is " + UserName);
+
+
+        }
+    }
+}
---
+++ C#/ConsoleApp1/ConsoleApp1/program.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstProg
+{
+    class Program
+    {
+        static void Main(string[] args) // "Main method", anything within the following curly bracets is the code of the program.
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("World!");
+
+            Console.Write("Hello");
+            Console.Write("World");
+
+            Console.WriteLine("\nA\nA");
+
+            Console.ReadKey();  // Prevents program from continueing until any key is pressed.
+            Console.Beep();
+        }
+    }
+}
