0c0b1f6ad7ce01a4cc03dbbaabf6a265f87f8ab5
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-05-10 03:24:24 +0300

    09

--- C#/ConsoleApp1/08_random_numbers/Program.cs
+++ C#/ConsoleApp1/08_random_numbers/Program.cs
@@ -6,7 +6,9 @@
         {
             Random x = new Random();  // Creating a 'Random' object.
 
-            int num = x.Next(1, 6);  // Generating a random number between 1 and 6. First arg is inclusive second arg is exclusive.
+            int num = x.Next(1, 7);  // Generating a random number between 1 and 6. First arg is inclusive second arg is exclusive.
+
+            double num1 = x.NextDouble();  // Generates a random float num between 0 and 1.
 
             Console.WriteLine(num);
         }
