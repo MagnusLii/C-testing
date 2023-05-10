1d0f24deacec601113593e1f5a27e726e61f4d81
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-05-11 01:22:26 +0300

    Added 02_escape_sequences.

---
+++ C/02_escape_sequences/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int main(){
+    //  escape sequence = character combination starting with a backslash \
+    //  followed by a letter or combination of digits.
+    
+    // \n newline.
+    printf("1\n2\n3\n");
+
+    // \t tab.
+    printf("1\t2\t3\n");
+
+    // \" or \' for quotes.
+    printf("\"Bees are too fat to fly\"");
+
+    // list of escape sequences in C.
+    // https://en.wikipedia.org/wiki/Escape_sequences_in_C
+
+    return 0;
+}
