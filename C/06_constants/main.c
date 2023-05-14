2edfd495dcf245a8631f49cc472d23e9c0aad503
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-05-14 04:12:23 +0300

    Added constants
    Short run through on constants and their best practices.

---
+++ C/06_constants/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    // constant = fixed value that cannot be altered by the program during its execution.
+
+    float pi = 3.14159;
+    const float PI = 3.14159;  // 'const' makes the value a constant. Constant names are traditionally in all caps.
+
+    pi = 420.69;
+    //PI = 420.69;  // this throwns an error.
+
+    printf("%f", pi);
+    printf("%f", PI);
+
+    return 0;
+}
