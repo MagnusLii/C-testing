70c9fc8b25d932a0f38d42779edf1eca8997b88a
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-05-14 04:56:20 +0300

    Add augmented assignment operators
    Just a demo of augmented assignment operators in C.

---
+++ C/08_augmented_assignment_operators/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+ 
+
+int main(){
+
+  
+
+    // augmented assignment operators = used to replace a statement where an operator
+    //                                  takes a variable as one of its arguments
+    //                                  and then assigns the result back to the same variable
+    //                                  x = x + 1
+    //                                  x+=1
+
+    int x = 10;
+
+    x = x + 2;
+    x+=2;
+    printf("%d\n", x);
+
+    x = x - 3;
+    x-=3;
+    printf("%d\n", x);
+
+    x = x * 4;
+    x*=4;
+    printf("%d\n", x);
+
+    x = x / 5;
+    x/=5;
+    printf("%d\n", x);
+
+    x = x % 2;
+    x%=2;
+    printf("%d\n", x);
+
+    
+    return 0;
+}
