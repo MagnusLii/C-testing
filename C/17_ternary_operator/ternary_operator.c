f214a09ce0620cbd855efd5bed6c482cf6f89b0e
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-05-19 14:28:59 +0300

    Create ternary_operator.c
    Short demo of ternary operator.

---
+++ C/17_ternary_operator/ternary_operator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// ternary operator = shortcut to if/else when assigning/returning a value
+// formula == (condition) ? value if true : value if false
+
+
+// findmax function with if statements.
+int findMax(int x, int y){
+    if (x > y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+    
+}
+
+// findmax function with ternary operator.
+int ternaryFindMax(int x, int y)
+{
+    return (x > y) ? x : y;  // returns 'x' if x>y and 'y' if x<y.
+}
+
+
+int main()
+{
+    int max = findMax(3, 4);
+    int max = ternaryFindMax(3, 4);
+
+    printf("%d", max);
+
+    return 0;
+}
