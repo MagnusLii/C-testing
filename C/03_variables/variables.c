7e44f7a4f7ea9f4c5182def677a4626730e12e86
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-05-19 14:16:59 +0300

    fixt typo.

--- C/03_variables/variables.c
+++ C/03_variables/variables.c
@@ -4,7 +4,7 @@ int main(){
 
     int x;       // declaration.
     x = 123;     //initialization.
-    int y = 321; // declaration 0 initialization.
+    int y = 321; // declaration + initialization.
 
     int age = 20;       // int.
     float gpa = 2.05;   // float.
