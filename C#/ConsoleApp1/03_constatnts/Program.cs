fdc441c542c67baaedda044cbdca8d839225426e
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-07-11 21:06:00 +0300

    redo.

---
+++ C#/ConsoleApp1/03_constatnts/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* constants =  immutable values which are known at compile time
+                            and do not change for the life of the program. */
+
+
+            double pie = 3.14159;
+            pie = 123;
+
+            Console.WriteLine(pie);
+
+            const double pi = 3.14159;  // const makes a variable a constant.
+            
+            //pi = pi * pi;  // Does not work.
+
+            Console.WriteLine(pi);
+
+
+        }
+    }
+}
---
+++ C#/ConsoleApp1/06_arithmetic_operators/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace arithmeic_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int friends = 10;
+
+            // Basic addition.
+            friends = friends + 1;
+            Console.WriteLine(friends);
+            friends += 1;
+            Console.WriteLine(friends);
+            friends++;  // This format always increases the number by 1, useful in loops.
+            Console.WriteLine(friends);
+
+            // Basic subtraction
+            friends = friends - 1;
+            Console.WriteLine(friends);
+            friends -= 1;
+            Console.WriteLine(friends);
+            friends--;
+            Console.WriteLine(friends);
+
+            // Multiplication.
+            friends = friends * 2;
+            Console.WriteLine(friends);
+            friends *= 2;
+            Console.WriteLine(friends);
+
+            // Subtraction
+            friends = friends / 3;  // Decimal portion will be trunkated cause the var is an int.
+            Console.WriteLine(friends);
+            friends /= 3;
+            Console.WriteLine(friends);
+
+            // Modulos
+            int remainder = friends % 3;  // Divides the value of 'friends' into groups of 3 and stores the leftover value once it can no longer be divided into groups of 3.
+                                          // As friends is 4 one group of 3 is removed and the since 1 < 3 the remainder is 2.
+            Console.WriteLine(remainder);
+        }
+    }
+}
